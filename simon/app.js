f3bcccee2a7f336d915ad83ade5f4d3260db1be9
jaxdsout <jaxon.southern@mac.com>
2023-09-07 21:52:56 -0500

    infrastructure started

---
+++ simon/app.js
@@ -0,0 +1,8 @@
+
+const yellow = document.querySelector(".yellow");
+const red = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+const green = document.querySelector(".green");
+const start = document.querySelector(".start-dot");
+const timer = document.querySelector(".timer");
+const slider = document.querySelector(".difficulty-slider");
